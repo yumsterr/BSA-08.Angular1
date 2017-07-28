f19fba32358a39b444638419f4b6793ae48688b7
Lina Vasylieva <Yumster@Yumster-mac.Dlink>
2017-07-28 09:25:08 +0300

    stopwatch. a little bit to finish

--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -1,43 +1,84 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ILapInfo {
+  total: number;
+  lap: number;
+  avg: number;
+  diff: number;
+  start: number;
+}
+
 @Component({
   selector: 'app-stopwatch',
   templateUrl: './stopwatch.component.html',
   styleUrls: ['./stopwatch.component.css']
 })
+
 export class StopwatchComponent implements OnInit {
   currentWatchPos = 0;
   minutes = 0;
   seconds = 0;
   mlseconds = 0;
   watchInterval: any;
+  showStart = true;
+  stopwatchHistory = [];
+  currentResRow = {} as ILapInfo;
+  avgLap = [];
 
   constructor() { }
 
   ngOnInit() {
-
+    console.log(this.stopwatchHistory);
   }
 
   startTimer() {
-    console.log(this.currentWatchPos);
+    this.currentResRow.start = this.currentWatchPos;
+    this.showStart = false;
     this.watchInterval = setInterval(() => {
       this.currentWatchPos += 10;
-      const {min, sec, mlsec} = this.getTime(this.currentWatchPos);
-      this.minutes = min;
-      this.seconds = sec;
-      this.mlseconds = mlsec;
+      this.showTime(this.currentWatchPos);
     }, 10);
   }
   pauseTimer() {
     clearInterval(this.watchInterval);
+    this.stopwatchHistory.push(this.currentResRow);
+    this.currentResRow = {} as ILapInfo;
+    this.showStart = true;
   }
-  resertTimer() { }
+  resertTimer() {
+    clearInterval(this.watchInterval);
+    this.currentWatchPos = 0;
+    this.showStart = true;
+    this.showTime(this.currentWatchPos);
+  }
 
-  getTime(mlseconds) {
-    const min = Math.trunc((mlseconds / 1000) / 60);
-    const sec = Math.trunc((mlseconds / 1000) % 60);
-    const mlsec = (mlseconds % 1000);
-    return {min, sec, mlsec};
+  lapTimer() {
+    // console.log(this.currentResRow.total);
+    const lapTime = this.currentWatchPos - this.currentResRow.start; // prev value
+    this.currentResRow.total = this.currentWatchPos;
+    this.currentResRow.lap = lapTime;
+    this.currentResRow.avg = this.average();
+    this.stopwatchHistory.push(this.currentResRow);
+    this.currentResRow = {} as ILapInfo;
+    this.currentResRow.start = this.currentWatchPos;
   }
+
+  showTime(mlseconds) {
+    this.minutes = Math.trunc((mlseconds / 1000) / 60);
+    this.seconds = Math.trunc((mlseconds / 1000) % 60);
+    this.mlseconds = (mlseconds % 1000);
+  }
+
+  average() {
+    let sum = this.currentResRow.lap;
+
+    for (let i = 0; i < this.stopwatchHistory.length; i++) {
+      const lapTime = this.stopwatchHistory[i].lap || 0;
+      sum += parseInt(lapTime, 10);
+    }
+    const avg = Math.round(sum / (this.stopwatchHistory.length + 1));
+    return avg;
+  }
+
 
 }
